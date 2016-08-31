211f7d99d253b85b5c283da61cd41eaa50d000b3
Сливинский Николай <factslives@ya.ru>
2016-09-01 00:43:45 +0300

    Update.

--- src/public/scripts/chat.js
+++ src/public/scripts/chat.js
@@ -1,8 +1,9 @@
 "use strict";
 (function($) {
     var socket = null;
-    var clientId = null;
     var nickname = null;
+    var windowActive = true;
+    var messageSong = '../public/sounds/sound.mp3';
     var serverProtocol = 'http';
     var serverAddress = 'localhost';
     var serverPort = '8080';
@@ -10,10 +11,7 @@
     function bindDOMEvents() {
         $('.usernameInput').keyup(function(){
             if(event.keyCode==13) {
-                nickname = $(this).val();
-                connect();
-                $('.login-page').fadeOut(200);
-                $('.message').focus();
+                handleNicname();
             }
         });
 
@@ -29,7 +27,7 @@
             var users = '';
 
             for(var i in data) {
-                users += '<div class="user">' + data[i] + '</div>';
+                users += '<div class="user" data="' + i + '"><span class="name">' + data[i] + '</span></div>';
             }
 
             $('.left-col').html(users);
@@ -38,18 +36,64 @@
         socket.on('newMessage', function(data){
             $(".messages").append('<div class="mess"><div class="mess-username">' + data.userName + '</div>' +
                 data.message + '</div>');
-        })
 
+            playMessageSong();
+
+            chatScroll();
+        });
+
+        socket.on('usersStatus', function(data){
+            $('.user').each(function(f,elem) {
+                var trigger = false;
+
+                for(var i in data) {
+                    if(i == $(this).attr("data")) {
+                        trigger = true;
+                    }
+                }
+
+                if (trigger) {
+                    $(this).find('.offline').remove();
+                    $(this).find('.online').remove();
+                    $(this).find('.name').append('<span class="online"></span>');
+                } else {
+                    $(this).find('.online').remove();
+                    $(this).find('.offline').remove();
+                    $(this).find('.name').append('<span class="offline"></span>');
+                }
+            });
+        })
     }
 
     function handleMessage(){
         var message = $('.message').val().trim();
+
         if(message){
             socket.emit('newMessage', {message: message});
+
             $(".messages").append('<div class="mess own"><div class="mess-username own">' + nickname + '</div>' +
-                message + '</div>')
+                message + '</div>');
+
             $('.message').val('');
+
+            chatScroll();
         }
+    }
+
+    function handleNicname(){
+        nickname = $('.usernameInput').val().trim();
+
+        if(nickname) {
+            connect();
+
+            $('.login-page').fadeOut(200);
+
+            $('.message').focus();
+        }
+    }
+
+    function chatScroll() {
+        $('.messages').scrollTop($('.messages').prop('scrollHeight'));
     }
 
     function connect(){
@@ -60,7 +104,34 @@
         socket.emit('userRegister', {"userName" : nickname});
     }
 
+    function playMessageSong() {
+        var audio = new Audio();
+        audio.src = messageSong;
+        audio.autoplay = true;
+    }
+
+    window.onblur = function () {
+        if(nickname && socket) {
+            if (windowActive) {
+                windowActive = false;
+
+                socket.emit('offline');
+            }
+        }
+    };
+
+    window.onfocus = function () {
+        if(nickname && socket) {
+            if(!windowActive) {
+                windowActive = true;
+
+                socket.emit('online');
+            }
+        }
+    };
+
     $(function(){
         bindDOMEvents();
     });
+
 })(jQuery);
--- src/server.js
+++ src/server.js
@@ -1,22 +1,39 @@
 var port = 8080;
 var chatClients = {};
+var chatClientsOnline = {};
 
 var io = require('socket.io').listen(port);
 
 io.sockets.on('connection', function(socket) {
     socket.on('userRegister', function(data){
         userRegister(socket, data);
-        io.sockets.emit('updateChatList', chatClients);
     });
 
     socket.on('newMessage', function(data) {
         newMessage(socket, data);
     });
+
+    socket.on('online', function(data) {
+        online(socket, data);
+    });
+
+    socket.on('offline', function(data) {
+        offline(socket, data);
+    });
+
+    socket.on('disconnect', function (data) {
+        disconnect(socket, data);
+    });
 });
 
 function userRegister(socket, data) {
     chatClients[socket.id] = data.userName;
+
     socket.username = data.userName;
+
+    io.sockets.emit('updateChatList', chatClients);
+
+    online(socket, data);
 }
 
 function newMessage(socket, data) {
@@ -24,6 +41,30 @@ function newMessage(socket, data) {
         userName: socket.username,
         message: data.message
     });
+}
+
+function disconnect(socket, data) {
+    setTimeout(function () {
+        delete chatClients[socket.id];
+        delete chatClientsOnline[socket.id];
+
+        io.sockets.emit('updateChatList', chatClients);
+    }, 500);
+}
+function online(socket, data) {
+    chatClientsOnline[socket.id] = socket.username;
+
+    socket.status = 'online';
+
+    io.sockets.emit('usersStatus', chatClientsOnline);
+}
+
+function offline(socket, data) {
+    delete chatClientsOnline[socket.id];
+
+    socket.status = 'offline';
+
+    io.sockets.emit('usersStatus', chatClientsOnline);
 }
 
 
